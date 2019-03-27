d8ea8497afc11f840d953e51c8fe94be73efb822
AlfiGoyal <alfigoyal@gmail.com>
2019-03-26 19:51:51 -0500

    Added pollyfill for IE support

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RetailApp from './retailApp';
 import './style.css';
+import '@babel/polyfill';
 
 ReactDOM.render(<RetailApp />, document.getElementById('root'));
